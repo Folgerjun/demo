c263b837f49f23c8468ddfd9a75423f2f0c951bc
DoubleFJ <ffj0721@163.com>
2018-01-19 14:54:20 +0800

    上传spring-boot-demo

---
+++ spring-boot-demo/demo/src/main/java/com/example/demo/controller/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+// @RestController 相当于 @Controller + @ResponseBody 都以json格式返回
+public class HelloWorld {
+
+	@RequestMapping("/hello")
+	public String hello() {
+
+		return "hello";
+
+	}
+
+}
